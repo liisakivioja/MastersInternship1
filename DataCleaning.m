17df6cb36b5898058851ca4d6178c7f2375ac686
liisakivioja <104033187+liisakivioja@users.noreply.github.com>
2022-04-21 12:06:50 +0200

    Create DataCleaning.m

---
+++ DataCleaning.m
@@ -0,0 +1,73 @@
+=============================================================================
+%% Clean the functional connectivity dataset by first disregarding unsuitable
+%% subjects and then applying the outlier criterion on subject's total FC and
+%% on intersubject correlation coefficients between functional connectivity
+%% matrices. 
+%%
+%% Start by loading the functional connectivity matrix and the volume matrix.
+=============================================================================
+  
+%% Filter out unsuitable matrices in subjects
+cmatrix = squeeze(connectivity(:,:,1,:));
+subjects = any(cmatrix,[1 2]);
+
+newconn = cmatrix(:,:,subjects==1); %disregard their connectivity matrices
+VOLS = regionProperties(:,4,subjects==1); %disregard their volumes
+
+%% Let's compute the total volume for each subject
+m = size(newconn, 3);
+vol = zeros(m,1);
+
+for i = 1:m
+    temp = sum(VOLS(:,:,i));
+    vol(i,:) = temp;
+end
+
+%% Clean the dataset from outliers
+%Average functional connectivity per subject
+for i = 1:size(newconn,3)
+    sl = (squareform(newconn(:,:,i)));
+    avg = mean(sl,'all'); 
+    fconn(i,:) = avg;
+end
+
+s = [1:size(newconn,3)].'; %create subjects list
+fconn = [fconn s];
+
+Q1 = quantile(fconn,[0.25]); 
+Q2 = quantile(fconn,[0.5]); 
+Q3 = quantile(fconn,[0.75]);
+IQR = iqr(fconn); 
+acf = fconn(fconn(:,1)<(Q1-1.5*IQR)|fconn(:,1)>(Q3+1.5*IQR),:);
+clear Q1 Q2 Q3 IQR
+  
+%% Find subjects with strongly deviating connectivity patterns
+T = zeros(m);
+J = newconn(:,:,:);
+J = permute(reshape(J, 114, 114, 470), [3 1 2]); %114 is the amount of nodes
+T = corr(J(:,:)'); 
+
+%Let's visualize T
+figure; imagesc(T); colorbar
+
+%% Get a single score per subject
+score = mean(T,2);
+subs = [1:size(score)].';
+scatter(subs,score);
+
+subscore = []; 
+subscore = [subs score];
+
+Q1 = quantile(score,[0.25]); 
+Q2 = quantile(score,[0.5]); 
+Q3 = quantile(score,[0.75]); 
+IQR = iqr(score);
+outl = subscore(subscore(:,2)<(Q1-1.5*IQR)|subscore(:,2)>(Q3+1.5*IQR),:); 
+
+%Let's disregard the subjects identified as potential outliers in both methods.
+newconn(:,:,118) = [];
+newconn(:,:,434) = [];
+vol(118) = [];
+vol(434) = [];
+subs = [1:size(newconn,3)].'; %update the amount of included subjects
+         
